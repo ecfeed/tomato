9f2b8b4194061196a1d5d81a41f9d00730e00443
Patryk Chamuczyński <p.chamuczynski@testify.no>
2023-11-27 10:24:39 +0100

    cosmetics

--- constraint_parser.py
+++ constraint_parser.py
@@ -3,76 +3,6 @@
 import pyparsing
 from pyparsing import *
 
-is_literal = CaselessLiteral("IS")
-not_literal = CaselessLiteral("NOT")
-in_literal = CaselessLiteral("IN")
-
-is_relation = is_literal
-is_not_relation = Combine(is_literal + not_literal, adjacent=False, joinString=' ')
-in_relation = in_literal
-not_in_relation = Combine(not_literal + in_literal, adjacent=False, joinString=' ')
-
-name = QuotedString('\'', escChar='\\') | QuotedString('"', escChar='\\')
-aggregated_name = Group(Suppress('[') + name + OneOrMore(Suppress(',') + name) + Suppress(']'))
-
-simple_statement = name + is_relation + name | name + is_not_relation + name
-aggregate_statement = name + in_relation + aggregated_name | name + not_in_relation + aggregated_name
-primitive_statement = Group(simple_statement | aggregate_statement)
-
-and_literal = CaselessLiteral("AND")
-or_literal = CaselessLiteral("OR")
-logical_operator = and_literal | or_literal
-logical_operator.setResultsName('logical_operator')
-expression = Group(infixNotation(primitive_statement, 
-    [
-        (not_literal, 1, opAssoc.RIGHT), 
-        (logical_operator, 2, opAssoc.LEFT), 
-        (or_literal, 2, opAssoc.LEFT)
-     ]))
-
-if_literal = CaselessLiteral("IF")
-then_literal = CaselessLiteral("THEN")
-implies_literal = CaselessLiteral("=>")
-implication = expression + Suppress(implies_literal) + expression | Suppress(if_literal) + expression + Suppress(then_literal) + expression
-invariant = expression
-constraint = implication | invariant
-
-class Expression:
-    pass
-
-class CompositeExpression(Expression):
-    def __init__(self, lval, operator, rval):
-        self.lval = parse_expression(lval)
-        self.operator = operator
-        self.rval = parse_expression(rval)
-        
-    def __str__(self):
-        return '(' + str(self.lval) + ' ' + self.operator + ' ' + str(self.rval) + ')'
-
-
-class NegatedExpression(Expression):
-    def __init__(self, tokens):
-        self.expression = parse_expression(tokens)
-        
-    def __str__(self):
-        return '(NOT ' + str(self.expression) + ')'
-
-class PrimitiveStatement:
-    def __init__(self, lval, operator, rval):
-        self.negated = False
-        if 'NOT' in operator:
-            self.negated = True
-        
-        self.lval = lval
-        
-        if 'IS' in operator:
-            self.allowed_values = [rval]
-        elif 'IN' in operator:
-            self.allowed_values = rval.asList()
-            
-    def __str__(self):
-        return '(' + str(self.lval) + (' NOT IN ' if self.negated else ' IN ') + str(self.allowed_values) + ')'
-
 def parse_expression(expression_tokens):
     while len(expression_tokens) == 1:
         expression_tokens = expression_tokens[0]
@@ -96,6 +26,39 @@ def parse_expression(expression_tokens):
         
         return PrimitiveStatement(lval_tokens, operator, rval_tokens)
 
+class CompositeExpression:
+    def __init__(self, lval, operator, rval):
+        self.lval = parse_expression(lval)
+        self.operator = operator
+        self.rval = parse_expression(rval)
+        
+    def __str__(self):
+        return '(' + str(self.lval) + ' ' + self.operator + ' ' + str(self.rval) + ')'
+
+
+class NegatedExpression:
+    def __init__(self, tokens):
+        self.expression = parse_expression(tokens)
+        
+    def __str__(self):
+        return '(NOT ' + str(self.expression) + ')'
+
+class PrimitiveStatement:
+    def __init__(self, lval, operator, rval):
+        self.negated = False
+        if 'NOT' in operator:
+            self.negated = True
+        
+        self.lval = lval
+        
+        if 'IS' in operator:
+            self.allowed_values = [rval]
+        elif 'IN' in operator:
+            self.allowed_values = rval.asList()
+            
+    def __str__(self):
+        return '(' + str(self.lval) + (' NOT IN ' if self.negated else ' IN ') + str(self.allowed_values) + ')'
+
 class Invariant:
     def __init__(self, expression_tokens):
         self.expression = parse_expression(expression_tokens)
@@ -111,8 +74,44 @@ class Implication:
     def __str__(self):
         return 'IF ' + str(self.precondition) + ' THEN ' + str(self.postcondition)
 
+
 def parse_constraint(constraint_string):
+    is_literal = CaselessLiteral("IS")
+    not_literal = CaselessLiteral("NOT")
+    in_literal = CaselessLiteral("IN")
+
+    is_relation = is_literal
+    is_not_relation = Combine(is_literal + not_literal, adjacent=False, joinString=' ')
+    in_relation = in_literal
+    not_in_relation = Combine(not_literal + in_literal, adjacent=False, joinString=' ')
+
+    name = QuotedString('\'', escChar='\\') | QuotedString('"', escChar='\\')
+    aggregated_name = Group(Suppress('[') + name + OneOrMore(Suppress(',') + name) + Suppress(']'))
+
+    simple_statement = name + is_relation + name | name + is_not_relation + name
+    aggregate_statement = name + in_relation + aggregated_name | name + not_in_relation + aggregated_name
+    primitive_statement = Group(simple_statement | aggregate_statement)
+
+    and_literal = CaselessLiteral("AND")
+    or_literal = CaselessLiteral("OR")
+    logical_operator = and_literal | or_literal
+    logical_operator.setResultsName('logical_operator')
+    expression = Group(infixNotation(primitive_statement, 
+        [
+            (not_literal, 1, opAssoc.RIGHT), 
+            (logical_operator, 2, opAssoc.LEFT), 
+            (or_literal, 2, opAssoc.LEFT)
+        ]))
+
+    if_literal = CaselessLiteral("IF")
+    then_literal = CaselessLiteral("THEN")
+    implies_literal = CaselessLiteral("=>")
+    implication = expression + Suppress(implies_literal) + expression | Suppress(if_literal) + expression + Suppress(then_literal) + expression
+    invariant = expression
+    constraint = implication | invariant
+
     tokens = constraint.parseString(constraint_string, parseAll=True)
+
     if len(tokens) == 1:
         # invariant
         return Invariant(tokens[0])
