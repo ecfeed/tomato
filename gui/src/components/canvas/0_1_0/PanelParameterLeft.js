5054b5e6a3ba6bef6ab4de0806fee2911e36e878
Krzysztof Skorupski <k.skorupski@testify.no>
2024-01-08 11:52:00 +0100

    update

--- gui/src/components/canvas/0_1_0/PanelParameterLeft.js
+++ gui/src/components/canvas/0_1_0/PanelParameterLeft.js
@@ -1,21 +1,21 @@
-import { ButtonAdd } from './ButtonAdd';
-import styles from './PanelParameterLeft.module.scss'
-import { useParameter } from './context/ParameterContext'
+import { ButtonAdd } from "./ButtonAdd";
+import styles from "./PanelParameterLeft.module.scss";
+import { useParameter } from "./context/ParameterContext";
 
 export function PanelParameterLeft() {
-    const { top, isLocked, handleAddParameterParent } = useParameter();
+  const { top, isLocked, handleAddParameterParent } = useParameter();
 
-    if (!top) {
-        return null;
-    }
+  if (!top) {
+    return null;
+  }
 
-    const handleInternalAddParameter = (input) => {
-        handleAddParameterParent(input);
-      };
+  const handleInternalAddParameter = (input) => {
+    handleAddParameterParent(input);
+  };
 
-    return (
-        <div className={isLocked ? styles["panel-parameter"] : styles["panel-parameter--active"]}>
-            <ButtonAdd handleAddMainParameter={handleInternalAddParameter} />
-        </div>
-    )
+  return (
+    <div className={isLocked ? styles["panel-parameter"] : styles["panel-parameter--active"]}>
+      <ButtonAdd handleAddMainParameter={handleInternalAddParameter} />
+    </div>
+  );
 }
